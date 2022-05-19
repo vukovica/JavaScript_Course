037c3f5daa6324f6b4d6960670b089b4541d0f7f
vukovica <anastasija.vukovic02@gmail.com>
2022-05-19 19:16:15 +0200

    added first files

---
+++ index.js
@@ -0,0 +1,14 @@
+var a = prompt("Unesite broj:");
+
+var unosVrednosti = prompt("Unesite broj:");
+
+var value1 = prompt("Ukupan broj predmeta u 1. godini");
+value1 = parseInt(value1);
+
+var value2 = prompt("Ukupan broj predmeta u 2. godini:");
+value2 = parseInt(value2);
+
+var ukupanBrojPredmeta = parseInt(value1) + parseInt(value2);
+
+var textZaConsolu = "Broj predmeta";
+
